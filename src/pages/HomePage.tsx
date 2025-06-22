57b6db66ae9cfa069c4fed1036b36c273203ec22
Lobster444 <misanya111@gmail.com>
2025-06-22 18:26:25 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -144,6 +144,11 @@ const HomePage: React.FC = () => {
       <FeaturesBenefits />
 
       {/* AI Video Tutor Demo */}
+      <AIVideoTutor
+        currentUser={currentUser}
+        onStartPracticing={handleStartLearningFree}
+      />
+
       {/* Featured Courses Section */}
       <FeaturedCoursesSection
         courses={courses}
@@ -164,6 +169,13 @@ const HomePage: React.FC = () => {
       )}
 
       {/* Customer Testimonials Section */}
+      <TestimonialsSection
+        currentUser={currentUser}
+        onStartLearning={handleStartLearningFree}
+        onUpgrade={handleUpgradeClick}
+        onExploreCourses={handleExploreCourses}
+      />
+
       {/* Course Detail Modal */}
       <CourseDetailModal
         isOpen={showCourseModal}
