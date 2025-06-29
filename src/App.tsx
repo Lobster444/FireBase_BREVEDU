65e7f33bf81abc6691744eaff08ff7c8513d2089
Lobster444 <misanya111@gmail.com>
2025-06-29 19:42:45 +0100

    Self-host custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './contexts/AuthContext';
+import FontLoader from './components/FontLoader';
 import { useAnalytics } from './hooks/useAnalytics';
 import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
@@ -17,37 +18,39 @@ const AnalyticsWrapper: React.FC<{ children: React.ReactNode }> = ({ children })
 };
 function App() {
   return (
-    <div className="font-sans antialiased">
-      <AuthProvider>
-        <Router>
-          <ScrollToTop />
-          <AnalyticsWrapper>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/courses" element={<CoursesPage />} />
-            <Route path="/brevedu-plus" element={<BrevEduPlusPage />} />
-            <Route path="/admin/courses" element={<AdminCoursesPage />} />
-          </Routes>
-          </AnalyticsWrapper>
-          
-          {/* Toast Container */}
-          <ToastContainer
-            position="top-center"
-            autoClose={4000}
-            hideProgressBar={true}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            toastClassName="bg-white border border-black/5 text-black font-sans rounded-[0.8rem] shadow-lg"
-            progressClassName="bg-hyper-yellow"
-          />
-        </Router>
-      </AuthProvider>
-    </div>
+    <FontLoader>
+      <div className="font-sans antialiased">
+        <AuthProvider>
+          <Router>
+            <ScrollToTop />
+            <AnalyticsWrapper>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/courses" element={<CoursesPage />} />
+              <Route path="/brevedu-plus" element={<BrevEduPlusPage />} />
+              <Route path="/admin/courses" element={<AdminCoursesPage />} />
+            </Routes>
+            </AnalyticsWrapper>
+            
+            {/* Toast Container */}
+            <ToastContainer
+              position="top-center"
+              autoClose={4000}
+              hideProgressBar={true}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              toastClassName="bg-white border border-black/5 text-black font-sans rounded-[0.8rem] shadow-lg"
+              progressClassName="bg-hyper-yellow"
+            />
+          </Router>
+        </AuthProvider>
+      </div>
+    </FontLoader>
   );
 }
 
