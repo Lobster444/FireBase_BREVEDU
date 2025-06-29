0ef5fb7c6fac9f37eb39ab1ef5a76890b5a7ea7f
Lobster444 <misanya111@gmail.com>
2025-06-29 23:33:22 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -189,7 +189,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage }) => {
                         variant="ghost"
                        size="xs"
                         onClick={handleLogout}
-                       className="text-left px-0 mt-1 justify-start text-xs min-h-[32px] -ml-5"
+                       className="text-left px-0 mt-1 justify-start text-xs min-h-[32px] ml-5"
                         icon={SignOut}
                       >
                         <span>Sign Out</span>
