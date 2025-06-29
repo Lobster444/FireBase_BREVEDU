b4e98d235bb2cdb93c512d6b191ec6e60fe552b7
Lobster444 <misanya111@gmail.com>
2025-06-29 23:30:23 +0100

    Fix Sign Out button alignment with text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -189,7 +189,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage }) => {
                         variant="ghost"
                        size="xs"
                         onClick={handleLogout}
-                       className="text-left px-2 mt-1 justify-start text-xs min-h-[32px]"
+                       className="text-left px-0 mt-1 justify-start text-xs min-h-[32px] -ml-2"
                         icon={SignOut}
                       >
                         <span>Sign Out</span>
@@ -320,7 +320,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage }) => {
                 console.log('🔄 handleLogout called');
                 setShowUserMenu(false);
               }}
-              className="text-left px-2 mt-1 justify-start text-xs min-h-[32px]"
+             className="text-left px-0 mt-1 justify-start text-xs min-h-[32px] -ml-2"
               icon={SignOut} 
             >
               <span>Sign Out</span>
