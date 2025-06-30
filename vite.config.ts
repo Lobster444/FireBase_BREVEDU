696a10ad099b1a9113c427106131ad3c28816797
Lobster444 <misanya111@gmail.com>
2025-06-30 12:31:41 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for assets to work in any deployment context
+  base: '/', // Use relative paths for assets to work in any deployment context
   plugins: [react()]
 });
